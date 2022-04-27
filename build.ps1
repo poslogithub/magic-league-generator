d321edd0d12b74055a7ec8b40b3584d1517bd408
poslogithub <black_tp@hotmail.com>
2022-04-28 06:02:53 +0900

    リンクを追加したりreadmeをなくしたりした

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
-PyInstaller --noconfirm --clean .\magic_league_generator.spec
-New-Item dist\sealed_generator\card_image -ItemType Directory
-New-Item dist\sealed_generator\set_data -ItemType Directory
-Remove-Item dist\sealed_generator.zip
-Compress-Archive -Path dist\sealed_generator -DestinationPath dist\sealed_generator.zip
+Remove-Item "dist\sealed_generator.zip"
+PyInstaller --noconfirm --clean ".\magic_league_generator.spec"
+New-Item "dist\sealed_generator\card_image" -ItemType Directory
+New-Item "dist\sealed_generator\set_data" -ItemType Directory
+Copy-Item -Path "*.url" -Destination "dist"
+Compress-Archive -Path "dist\*" -DestinationPath "dist\sealed_generator.zip"
